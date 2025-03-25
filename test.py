4569bde7d9d18df6d22f6eb552b8cca02ec93181
NoahP-K <noahpk@bu.edu>
2025-03-25 17:11:52 -0400

    Whoops, accidentally overwrote a previous commit during merging. Fixed that.

--- test.py
+++ test.py
@@ -57,11 +57,9 @@ for i in range(noisyVectorCount):
     noisyWorkload = []
     for w in [workload.z0, workload.z1, workload.q, workload.w]:
         wScaled = w * workloadScaler
-        while True:
-            noise = np.random.laplace(0, b, 1)[0] * noiseScaler
-            if wScaled + noise >= 0:
-                noisyWorkload.append((wScaled + noise) / workloadScaler)
-                break
+        noise = np.random.laplace(0, b, 1)[0] * noiseScaler
+        noisyW = (wScaled + noise) / workloadScaler
+        noisyWorkload.append(max(0, noisyW))
         
     nratio = 1 / sum(noisyWorkload)
     adjustedNoisyWorkload = [i * nratio for i in noisyWorkload]
