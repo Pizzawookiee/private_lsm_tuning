48c98d871107f034982cb77334cf31ce77f94cc4
NoahP-K <noahpk@bu.edu>
2025-03-25 17:03:07 -0400

    Added calculation of average workload and rho.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import math
 import numpy as np
 from pprint import pprint
 
@@ -13,6 +14,26 @@ from endure.lsm import (
     LSMDesign
 )
 from endure.solver import ClassicSolver
+
+def getAvgVector(vectorList):
+    if len(vectorList) == 0:
+        return []
+    entryNum = len(vectorList[0])
+    result = []
+    for i in range(entryNum):
+        sum = 0
+        for j in vectorList:
+            sum += j[i]
+        result.append(sum/len(vectorList))
+    return result
+
+def getMaxKLDistance(p, q):
+    if len(p) != len(q):
+        return -1
+    result = sum([(p[i]*np.log(p[i]/q[i])) for i in range(len(p))])
+    return result
+
+
 
 bounds = LSMBounds()
 gen = ClassicGen(bounds, seed=42)
@@ -29,17 +50,36 @@ b = sensitivity/epsilon
 workloadScaler = 1000
 noiseScaler = 1
 
-noisyWorkload = []
-for w in [workload.z0, workload.z1, workload.q, workload.w]:
-    wScaled = w * workloadScaler
-    while True:
-        noise = np.random.laplace(0, b, 1) * noiseScaler
-        if wScaled + noise >= 0:
-            noisyWorkload.append((wScaled + noise) / workloadScaler)
-            break
-    
-nratio = 1 / sum(noisyWorkload)
-adjustedNoisyWorkload = [i * nratio for i in noisyWorkload]
+noisyVectorCount = 10
+noisyVectors = []
 
-pprint(noisyWorkload)
-pprint(adjustedNoisyWorkload)
+for i in range(noisyVectorCount):
+    noisyWorkload = []
+    for w in [workload.z0, workload.z1, workload.q, workload.w]:
+        wScaled = w * workloadScaler
+        while True:
+            noise = np.random.laplace(0, b, 1)[0] * noiseScaler
+            if wScaled + noise >= 0:
+                noisyWorkload.append((wScaled + noise) / workloadScaler)
+                break
+        
+    nratio = 1 / sum(noisyWorkload)
+    adjustedNoisyWorkload = [i * nratio for i in noisyWorkload]
+
+    noisyVectors.append(adjustedNoisyWorkload)
+
+#pprint(noisyVectors)
+
+#priv = ((adjustedNoisyWorkload[0]-1) / adjustedNoisyWorkload[0]) < math.e**epsilon
+#pprint(priv)
+
+avgVector = getAvgVector(noisyVectors)
+print("average noisy vector: ", end = "")
+pprint(avgVector)
+
+maxKLDistance = -1
+for vector in noisyVectors:
+    d = getMaxKLDistance(avgVector, vector)
+    if d > maxKLDistance:
+        maxKLDistance = d
+print("max KL distance (rho): " + str(maxKLDistance))
