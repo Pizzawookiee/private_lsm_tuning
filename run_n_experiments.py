fixed best robust tuning calculation, use a perturbed workload instead of the average, added data for max expected rho and average expected rho

run_n_experiments.py
@@ -7,11 +7,12 @@ from workload_types import ExpectedWorkload
 import numpy as np
 import os
 import csv
+import time 
 
 ###############################################
 #    ROBUST DESIGN SOLVER ARGS
 ###############################################
-NUM_TRIALS = 100      # number of robust designs we try 
+NUM_TUNINGS = 100      # number of robust designs we try 
 
 ###############################################
 #    LAPLACE MECHANISM ARGS
@@ -23,9 +24,9 @@ SENSITIVITY = 1              # amount the function's output will change when its
 ###############################################
 #    EXPERIMENT SETUP (check workload_types)
 ###############################################
-workloadTypes    = [ExpectedWorkload.TRIMODAL_4]         
-
-subdirectory     = "experiment_results"                                           # save results to the correct folder
+#workloadTypes    = list(ExpectedWorkload)                                         # loads all workloads 
+workloadTypes    = [ExpectedWorkload.UNIMODAL_2]
+subdirectory     = "experiment_results/max_expected_rho"                          # save results to the correct folder
 os.makedirs(subdirectory, exist_ok=True)
 numWorkloads     = 10
 epsilonStart     = 0.05                                                           # epsilon start (inclusive)
@@ -34,6 +35,8 @@ stepSize         = 0.05
 
 
 for workloadType in workloadTypes: 
+    start_time = time.time()
+
     # setup file 
     file_name = str(workloadType) + ".csv"
     file_path = os.path.join(subdirectory, file_name)   
@@ -41,19 +44,26 @@ for workloadType in workloadTypes:
     originalWorkload = workloadType.workload
 
     table = []
-    header = ["Epsilon", "Robust Cost", "Nominal Cost", "Rho", "KL Distance"]
+    header = ["Epsilon", "Robust Cost", "Nominal Cost", "Rho (Expected)", "Rho (True)"]
     table.append(header)
 
     # run trials 
     for epsilon in np.arange(epsilonStart, epsilonEnd, stepSize): 
         trial = nWorkloadsTrial(originalWorkload=originalWorkload, epsilon=epsilon, 
                                 workloadScaler=WORKLOAD_SCALER, noiseScaler=NOISE_SCALER, 
-                                sensitivity=SENSITIVITY, numWorkloads=numWorkloads)
+                                sensitivity=SENSITIVITY, numWorkloads=numWorkloads, rhoMethod='max')
         
-        designNominal, designRobust, nominalCost, robustCost = trial.run_trial(numTrials=NUM_TRIALS, printResults=False)
-        table.append([epsilon, robustCost, nominalCost, trial.rho, trial.KLDistance])
+        designNominal, designRobust, nominalCost, robustCost = trial.run_trial(numTunings=NUM_TUNINGS)
+        table.append([epsilon, robustCost, nominalCost, trial.rhoExpected, trial.rhoTrue])
 
     with open(file_path, "w", newline='') as file:
         writer = csv.writer(file)
         writer.writerows(table)
     
+
+    
+    end_time = time.time()  # End timer
+    print(f"{workloadType} trial: {end_time - start_time:.4f} seconds")
+    
+
+

trials/n_workloads.py
@@ -14,120 +14,183 @@ from endure.lsm import (
     Workload
 )
 from differential_privacy import LaplaceMechanism
-from pprint import pprint 
 import numpy as np
 import warnings
-warnings.filterwarnings('ignore')
 
 class nWorkloadsTrial: 
+    """
+        Initializes an experimental trial 
+        Inputs: 
+         - originaWorkload: original workload 
+         - epsilon: level of noise 
+         - workload scaler: used for the Laplace mechanism (we will convert percentages to raw numbers)
+         - noise scaler: used for Laplace mechanism (scales noise)
+         - sensitivity: used for Laplace mechanism 
+         - numWorkloads: number of workloads generated to calculated rhoExpected 
+         - rhoMethod: method used to calculate expected rho (either average or max)
+
+        Attributes
+         - originalWorkload: true workload (hidden from the robust tuner)
+         - epsilon: level of noisy for Laplace mechanism 
+         - perturbedWorkload: workload perturbed using the laplace mechanism 
+         - rhoExpected: expected rho supplied to the robust tuner
+         - rhoTrue: true rho between originalWorkload and perturbedWorkload 
+    """
     def __init__(self, originalWorkload: Workload, epsilon:float, workloadScaler:int, noiseScaler:int, 
-                 sensitivity:float=1, numWorkloads:int=10):
+                 sensitivity:float=1, numWorkloads:int=10, rhoMethod: str='max'):
         self.originalWorkload = originalWorkload
         self.epsilon = epsilon
-        self.noisyWorkloads = self.getNoisyWorkloads(workload=originalWorkload, sensitivity=sensitivity, 
-                                                  epsilon=epsilon, noiseScaler=noiseScaler, workloadScaler=workloadScaler, 
-                                                  numWorkloads=numWorkloads)
-        self.noisyWorkload = self.getAverageNoisyWorkload(self.noisyWorkloads)
-        self.MaxKLDistance = self.getMaxKLDistance(self.noisyWorkloads, self.originalWorkload)
-        self.KLDistance = self.findKLDistance(self.noisyWorkload, self.originalWorkload)
-        self.rho = self.MaxKLDistance
+        self.perturbedWorkload = self.get_perturbed_workload(originalWorkload=originalWorkload, sensitivity=sensitivity, 
+                                                epsilon=epsilon, noiseScaler=noiseScaler, workloadScaler=workloadScaler)
+        self.rhoExpected = self.get_expected_rho(originalWorkload=originalWorkload, sensitivity=sensitivity, 
+                                         epsilon=epsilon, noiseScaler=noiseScaler, workloadScaler=workloadScaler, 
+                                         numWorkloads=numWorkloads, method=rhoMethod)
+        self.rhoTrue = self.find_KL(originalWorkload, self.perturbedWorkload)
+        
 
-    def get_best_robust_tuning(self, bounds: LSMBounds, numTrials, solver, system, costFunc): 
-        best_cost = np.inf
-        bestDesign = None
-        for i in range(numTrials): 
-            H = np.random.randint(bounds.bits_per_elem_range[0], bounds.bits_per_elem_range[1])
-            T = np.random.uniform(bounds.size_ratio_range[0], bounds.size_ratio_range[1])
-            LAMBDA = np.random.uniform(0, 10)
-            ETA = np.random.uniform(0, 10)
-            designRobust, scipy_opt_obj_robust = solver.get_robust_design(system, self.originalWorkload, rho=self.MaxKLDistance, 
-                                                                      init_args=[H, T, LAMBDA, ETA])
-            current_cost = costFunc.calc_cost(designRobust, system, self.originalWorkload)
-            if current_cost < best_cost: 
-                best_cost = current_cost
-                bestDesign = designRobust
-                
-        return best_cost, bestDesign
-    
-    def run_trial(self, numTrials:int=10, printResults: bool=True): 
+    """
+        Runs one experimental trial 
+        numTunings: the number of robust trials done before choosing the robust trial with the lowest cost
+    """
+    def run_trial(self, numTunings:int=10): 
         bounds = LSMBounds()
         gen = ClassicGen(bounds, seed=42)
         system = gen.sample_system()
         solver = ClassicSolver(bounds)
+        costCalculator = Cost(bounds.max_considered_levels)
 
-        # find ideal tunings
+        # find ideal tuning
         designNominal, scipy_opt_obj_ideal = solver.get_nominal_design(system, self.originalWorkload)
+        nominalCost = costCalculator.calc_cost(designNominal, system, self.originalWorkload)
 
-        # find cost
-        # the lower the cost the better
-        cost = Cost(bounds.max_considered_levels)
-        nominalCost = cost.calc_cost(designNominal, system, self.originalWorkload)
-
-        robustCost, designRobust = self.get_best_robust_tuning(bounds=bounds, numTrials=numTrials, solver=solver, 
-                                                               system=system, costFunc=cost)
-
-        # print results 
-        if (printResults):
-            print(f"{'  Nominal LSMDesign':20}")
-            print(f"    Bits per elem   : {designNominal.bits_per_elem:.4f}")
-            print(f"    Size ratio      : {designNominal.size_ratio:.2f}")
-            print(f"    Policy          : {designNominal.policy.name}")
-            print(f"    Kapacity        : {designNominal.kapacity}")
-
-            print(f"{'  Robust LSMDesign':20}")
-            print(f"    Bits per elem   : {designRobust.bits_per_elem:.4f}")
-            print(f"    Size ratio      : {designRobust.size_ratio:.4f}")
-            print(f"    Policy          : {designRobust.policy.name}")
-            print(f"    Kapacity        : {designRobust.kapacity}")
-
-            print()
-            print("COST")
-            print(f"{'  Nominal':20}: {nominalCost:.6f}")
-            print(f"{'  Robust':20}: {robustCost:.6f}")
+        # find best robust tuning 
+        designRobust = self.get_best_robust_tuning(bounds=bounds, numTunings=numTunings, solver=solver, system=system, costFunc=costCalculator)
+        # find the true cost of the robust tuning using the original workload
+        robustCost = costCalculator.calc_cost(designRobust, system, self.originalWorkload)
         return designNominal, designRobust, nominalCost, robustCost
-
-    def getNoisyWorkloads(self, workload, numWorkloads: int, noiseScaler, sensitivity, epsilon, workloadScaler):
-        mechanism = LaplaceMechanism(workloadScaler=workloadScaler, noiseScaler=noiseScaler, sensitivity=sensitivity, epsilon=epsilon)
-        workload = workloadToList(workload)
-        noisyVectors = []
-        for i in range(numWorkloads):
-            noisyWorkload = []
-            noisyWorkload = mechanism.perturb(workload)
-            noisyWorkload = listToWorkload(noisyWorkload)
-            noisyVectors.append(noisyWorkload)
-
-        return noisyVectors
     
-    def findKLDistance(self, w1, w2): 
+    
+
+    """
+        Find the best (lowest cost) out of n robust tunings. 
+        The robust tuner does not have access to the original workload, which means it 
+        also does not have access to the true rho 
+    """
+    def get_best_robust_tuning(self, bounds: LSMBounds, numTunings, solver, system, costFunc): 
+        best_cost = np.inf
+        bestDesign = None
+        costs = []
+
+        # repeat until we find a valid result
+        while best_cost == np.inf: 
+            for _ in range(numTunings): 
+                # Randomly choose init args for the tuner 
+                H = np.random.randint(bounds.bits_per_elem_range[0], bounds.bits_per_elem_range[1])
+                T = np.random.uniform(bounds.size_ratio_range[0], bounds.size_ratio_range[1])
+                LAMBDA = np.random.uniform(0, 10)
+                ETA = np.random.uniform(0, 10)
+
+                with warnings.catch_warnings(record=True) as caught_warnings:
+                    warnings.simplefilter("always", category=RuntimeWarning)  
+
+                    designRobust, scipy_opt_obj_robust = solver.get_robust_design(
+                        system, self.perturbedWorkload, rho=self.rhoExpected, 
+                        init_args=[H, T, LAMBDA, ETA]
+                    )
+
+                    # Cost is calculated based on the perturbed workload (expected cost)
+                    current_cost = costFunc.calc_cost(designRobust, system, self.perturbedWorkload)
+
+                    if any("overflow" in str(w.message).lower() for w in caught_warnings):
+                        #print("Skip tuning")
+                        continue
+                    else: 
+                        costs += [current_cost]
+
+                    # update best cost if no warnings were caught 
+                    if (current_cost < best_cost): 
+                        best_cost = current_cost
+                        bestDesign = designRobust
+                   
+        return bestDesign
+    
+
+    """
+        Finds the expected rho through a list of n workloads 
+        Default is choosing the one with the greatest KL divergence 
+    """
+    def get_expected_rho(self, originalWorkload, sensitivity, epsilon, noiseScaler, workloadScaler, numWorkloads, method:str="max"): 
+        # generate a list of n different workloads 
+        n_workloads = self.get_n_perturbed_workloads(originalWorkload, numWorkloads, noiseScaler, sensitivity, epsilon, workloadScaler)
+
+        if method == 'avg': 
+            expectedRho = self.get_avg_KL(n_workloads, originalWorkload)
+        else: 
+            expectedRho = self.get_max_KL(n_workloads, originalWorkload)
+
+        return expectedRho
+
+
+    """
+        Produces one perturbed workload 
+    """
+    def get_perturbed_workload(self, originalWorkload, noiseScaler, sensitivity, epsilon, workloadScaler):
+        mechanism = LaplaceMechanism(workloadScaler=workloadScaler, noiseScaler=noiseScaler, sensitivity=sensitivity, epsilon=epsilon)
+        originalWorkload = workloadToList(originalWorkload)
+        perturbedWorkload = mechanism.perturb(originalWorkload)
+        perturbedWorkload = listToWorkload(perturbedWorkload)
+        return perturbedWorkload
+    
+
+    """
+        Produces n perturbed workloads
+    """
+    def get_n_perturbed_workloads(self, originalWorkload, numWorkloads: int, noiseScaler, sensitivity, epsilon, workloadScaler): 
+        perturbedWorkloads = []
+        for i in range(numWorkloads): 
+            pWorkload = self.get_perturbed_workload(originalWorkload=originalWorkload, noiseScaler=noiseScaler, sensitivity=sensitivity, epsilon=epsilon, workloadScaler=workloadScaler)
+            perturbedWorkloads.append(pWorkload)
+        return perturbedWorkloads
+
+
+    """
+        Wrapper method that converts workload types to list first
+    """
+    def find_KL(self, w1, w2): 
         w1=workloadToList(w1)
         w2=workloadToList(w2)
-        return self.getKLDistance(w1, w2)
+        return self.get_KL(w1, w2)
     
-    def getKLDistance(self, p, q):
+
+    """
+        KL distance between two probability distributions 
+    """
+    def get_KL(self, p, q):
         if len(p) != len(q):
-            return -1
+            raise ValueError("Lists must have the same length.")
         result = sum([(p[i]*np.log(p[i]/q[i])) for i in range(len(p))])
         return result
-    
-    def getAverageNoisyWorkload(self, workloads: list) -> Workload: 
-        if len(workloads) == 0:
-            return []
-        vectorList = workloadListToListOfLists(workloads)
-        entryNum = len(vectorList[0])
-        result = []
 
-        for i in range(entryNum):
-            sum = 0
-            for j in vectorList:
-                sum += j[i]
-            result.append(sum/len(vectorList))
 
-        return Workload(z0=result[0], z1=result[1], q=result[2], w=result[3]) 
-
-    def getMaxKLDistance(self, noisyWorkloads, originalWorkload): 
+    """
+        finds the maximum KL divergence between perturbed workloads and
+        the original workload
+    """
+    def get_max_KL(self, perturbedWorkloadList, originalWorkload): 
         maxKLDistance = -np.inf
-        for workload in noisyWorkloads:
-            d = self.findKLDistance(originalWorkload, workload)
+        for workload in perturbedWorkloadList:
+            d = self.find_KL(originalWorkload, workload)
             if d > maxKLDistance:
                 maxKLDistance = d
         return maxKLDistance
+    
+
+    """
+        averages the KL divergence amongst n workloads 
+    """
+    def get_avg_KL(self, perturbedWorkloadList, originalWorkload): 
+        distanceSum = 0
+        for workload in perturbedWorkloadList:
+            distanceSum += self.find_KL(originalWorkload, workload)
+            
+        return distanceSum/(len(perturbedWorkloadList))